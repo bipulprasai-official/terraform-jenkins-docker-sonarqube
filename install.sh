cfb00040d367a03e01f15d46eb565bfded7d0c9c
Bipul Prasai <102454981+bipulprasai-official@users.noreply.github.com>
2023-12-13 11:50:14 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,42 @@ newgrp docker
 sudo chmod 777 /var/run/docker.sock
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
+# Install kubectl
+sudo apt update
+sudo apt install curl -y
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#install python
+sudo apt update
+sudo apt install python3 -y
+
+#install postgresql
+#!/bin/bash
+# Update the package list and install PostgreSQL
+sudo apt update
+sudo apt install -y postgresql postgresql-contrib
+# Start the PostgreSQL service and enable it on boot
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
+#mongoDB
+sudo apt update
+sudo apt install wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release -y
+curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
+sudo apt install mongodb-org -y
+sudo systemctl enable --now mongod
+sudo systemctl status mongod
+mongod --version
+
 #install trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
